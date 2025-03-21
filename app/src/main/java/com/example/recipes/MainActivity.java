da87859eebc6bc3de91707db7eedcf6f0373985f
tarla <Tarlavina50@mail.ru>
2025-03-21 19:10:11 +0700

    + Картинки

--- app/src/main/java/com/example/recipes/MainActivity.java
+++ app/src/main/java/com/example/recipes/MainActivity.java
@@ -3,16 +3,22 @@ package com.example.recipes;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-     TextView tvMealName, tvMealCategory, tvMealInstructions;
-     Button btnGetRecipe;
+    private TextView tvMealName, tvMealCategory, tvMealInstructions;
+    private ImageView ivMealThumb;
+    private Button btnGetRecipe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         tvMealName = findViewById(R.id.tvMealName);
         tvMealCategory = findViewById(R.id.tvMealCategory);
         tvMealInstructions = findViewById(R.id.tvMealInstructions);
+        ivMealThumb = findViewById(R.id.ivMealThumb);
         btnGetRecipe = findViewById(R.id.btnGetRecipe);
 
         btnGetRecipe.setOnClickListener(new View.OnClickListener() {
@@ -40,9 +47,16 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<MealResponse> call, Response<MealResponse> response) {
                 if (response.isSuccessful() && response.body() != null && response.body().getMeals() != null) {
                     Meal meal = response.body().getMeals().get(0);
+
+                    // Устанавливаем текст
                     tvMealName.setText(meal.getStrMeal());
                     tvMealCategory.setText(meal.getStrCategory());
                     tvMealInstructions.setText(meal.getStrInstructions());
+
+                    // Загружаем изображение с помощью Glide
+                    Glide.with(MainActivity.this)
+                            .load(meal.getStrMealThumb())
+                            .into(ivMealThumb);
                 }
             }
 
